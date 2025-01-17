598e6a01a7442efaf4eff262b4b968b47952a14f
Gabriel Costa <gabriel.costa@zup.com.br>
2025-01-17 15:41:40 -0300

    feat(Pagamento): Adiciona classe Pagamento
            Cria classe Pagamento com os campos id, idPedido, formaPagamento, valorPago, dataHoraPagamento e pedido.
            Implementa métodos getters, exceto para o campo pedido.
            Implementa
            método validatePayment para validar o pagamento

---
+++ src/main/java/com/zup/pizzaria/models/Pagamento.java
@@ -0,0 +1,50 @@
+package com.zup.pizzaria.models;
+
+import java.time.LocalDateTime;
+
+final public class Pagamento {
+	private final long id;
+	private final long pedidoId;
+	private final String formaPagamento;
+	private final double valorPago;
+	private final LocalDateTime dataHoraPagamento;
+private final  Pedido pedido;
+
+	public Pagamento(double valorPago, String formaPagamento, long pedidoId, Pedido pedido) {
+		double valorPedido = pedido.getValorPedido();
+validatePayment(valorPago, valorPedido);
+
+		this.valorPago = valorPago;
+		this.formaPagamento = formaPagamento;
+		this.pedidoId = pedidoId;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public long getPedidoId() {
+		return pedidoId;
+	}
+
+	public String getFormaPagamento() {
+		return formaPagamento;
+	}
+
+	public double getValorPago() {
+		return valorPago;
+	}
+
+	public LocalDateTime getDataHoraPagamento() {
+		return dataHoraPagamento;
+	}
+
+	private void validatePayment(double paymentAmount, double orderAmount){
+
+		if (paymentAmount < orderAmount){
+			throw new IllegalArgumentException("O valor do pagamento precisa ser igual ao valor do pedido");
+		}
+
+	}
+
+}
